27637e6adaed4b20ab8a1d52ffd7029b936ba7fb
Matthew Linnell <mlinnell@gmail.com>
2013-09-24 23:50:11 -0500

    first pass on peridoci checking of data and saving to file for comparison on next lookup

--- OpenBank.Client/Entities/Statement.cs
+++ OpenBank.Client/Entities/Statement.cs
@@ -1,16 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OpenBank.Client.Entities
 {
     public class Statement
     {
+        private static TextInfo TextInfo = new CultureInfo("en-US", false).TextInfo;
+
+        public Statement()
+        {
+            Transactions = new List<Transaction>();
+        }
+
         public Balance LedgerBalance { get; set; }
         public Balance AvailableBalance { get; set; }
         public List<Transaction> Transactions { get; set; }
+
+        public static Statement Parse(string json)
+        {
+            if (String.IsNullOrEmpty(json))
+                return new Statement();
+
+            JObject result = JsonConvert.DeserializeObject(json) as JObject;
+
+            var jStatement = result.First.First;
+            var statement = new Statement();
+            statement.LedgerBalance = new Balance()
+            {
+                Amount = Convert.ToDecimal(jStatement[Mappings.LedgerBalance][Mappings.Amount].ToString()),
+                Date = jStatement[Mappings.LedgerBalance][Mappings.Date].ToString(),
+            };
+
+            statement.AvailableBalance = new Balance()
+            {
+                Amount = Convert.ToDecimal(jStatement[Mappings.AvailableBalance][Mappings.Amount].ToString()),
+                Date = jStatement[Mappings.AvailableBalance][Mappings.Date].ToString(),
+            };
+
+            foreach (JObject nestedObj in jStatement[Mappings.Transactions] as JArray)
+            {
+                statement.Transactions.Add(new Transaction()
+                {
+                    ID = nestedObj[Mappings.ID].ToString(),
+                    Name = nestedObj[Mappings.Name].ToString(),
+                    Type = (TransactionType)Enum.Parse(typeof(TransactionType), TextInfo.ToTitleCase(nestedObj["type"].ToString().ToLower())),
+                    Date = nestedObj[Mappings.Date].ToString(),
+                    Amount = Convert.ToDecimal(nestedObj[Mappings.Amount])
+                });
+            }
+
+            return statement;
+        }
 
         public override string ToString()
         {
--- OpenBank.Client/Program.cs
+++ OpenBank.Client/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -14,63 +15,59 @@ namespace OpenBank.Client
 {
     public class Program
     {
-        private static TextInfo TextInfo = new CultureInfo("en-US", false).TextInfo;
+        private const string OutFile = @"C:\temp\credit.out";
 
         public static void Main(string[] args)
         {
-            // Use accounts.sensitive.template as an example
-            // In this git repo, any file ending in ".sensitive" is ignored.
-            JObject hash = JsonConvert.DeserializeObject(File.ReadAllText(@"C:\temp\accounts.sensitive")) as JObject;
-            var postParameters = hash.First.First.ToObject<Dictionary<string, string>>();
-
-            StringBuilder postData = new StringBuilder();
-            foreach (var kvp in postParameters)
-                if (String.IsNullOrEmpty(postParameters[kvp.Key]) == false)
-                    postData.AppendFormat("{0}={1}&", kvp.Key, kvp.Value);
-
-            byte[] data = Encoding.ASCII.GetBytes(postData.ToString().TrimEnd('&'));
-
-            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create("http://localhost:1234/statement");
-            httpReq.Method = "POST";
-            httpReq.ContentType = "application/x-www-form-urlencoded";
-            httpReq.ContentLength = data.Length;
-            httpReq.Accept = "application/json";
-
-            using (var stream = httpReq.GetRequestStream())
+            while (true)
             {
-                stream.Write(data, 0, data.Length);
-            }
+                // Use accounts.sensitive.template as an example
+                // In this git repo, any file ending in ".sensitive" is ignored.
+                JObject hash = JsonConvert.DeserializeObject(File.ReadAllText(@"C:\temp\accounts.sensitive")) as JObject;
+                hash["date_end"] = DateTime.Now.ToString("yyyyMMdd");
+                var postParameters = hash.First.First.ToObject<Dictionary<string, string>>();
 
-            HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse();
-            string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                StringBuilder postData = new StringBuilder();
+                foreach (var kvp in postParameters)
+                    if (String.IsNullOrEmpty(postParameters[kvp.Key]) == false)
+                        postData.AppendFormat("{0}={1}&", kvp.Key, kvp.Value);
 
-            JObject result = JsonConvert.DeserializeObject(responseString) as JObject;
+                byte[] data = Encoding.ASCII.GetBytes(postData.ToString().TrimEnd('&'));
 
-            var jStatement = result.First.First;
-            var statement = new Statement();
-            statement.LedgerBalance = new Balance()
-            {
-                Amount = Convert.ToDecimal(jStatement[Mappings.LedgerBalance][Mappings.Amount].ToString()),
-                Date = jStatement[Mappings.LedgerBalance][Mappings.Date].ToString(),
-            };
+                HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create("http://localhost:1234/statement");
+                httpReq.Method = "POST";
+                httpReq.ContentType = "application/x-www-form-urlencoded";
+                httpReq.ContentLength = data.Length;
+                httpReq.Accept = "application/json";
 
-            statement.AvailableBalance = new Balance()
-            {
-                Amount = Convert.ToDecimal(jStatement[Mappings.AvailableBalance][Mappings.Amount].ToString()),
-                Date = jStatement[Mappings.AvailableBalance][Mappings.Date].ToString(),
-            };
-
-            statement.Transactions = new List<Transaction>();
-            foreach (JObject nestedObj in jStatement[Mappings.Transactions] as JArray)
-            {
-                statement.Transactions.Add(new Transaction()
+                using (var stream = httpReq.GetRequestStream())
                 {
-                    ID = nestedObj[Mappings.ID].ToString(),
-                    Name = nestedObj[Mappings.Name].ToString(),
-                    Type = (TransactionType)Enum.Parse(typeof(TransactionType), TextInfo.ToTitleCase(nestedObj["type"].ToString().ToLower())),
-                    Date = nestedObj[Mappings.Date].ToString(),
-                    Amount = Convert.ToDecimal(nestedObj[Mappings.Amount])
-                });
+                    stream.Write(data, 0, data.Length);
+                }
+
+                HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse();
+                string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                string existingJson = File.ReadAllText(OutFile);
+                Statement existing = Statement.Parse(existingJson);
+                Statement updated = Statement.Parse(responseString);
+
+                if (updated.Transactions.Count > existing.Transactions.Count)
+                {
+                    var updatedHash = updated.Transactions.ToDictionary(item => item.ID, item => item);
+                    var existingHash = existing.Transactions.ToDictionary(item => item.ID, item => item);
+
+                    var newKeys = updatedHash.Keys.Where(key => existingHash.ContainsKey(key) == false);
+                    foreach (var key in newKeys)
+                    {
+                        var transaction = updatedHash[key];
+                        File.WriteAllText(@"C:\temp\" + transaction.ID, transaction.ToString());
+                    }
+                }
+
+                File.WriteAllText(OutFile, responseString);
+
+                Thread.Sleep(TimeSpan.FromHours(1));
             }
         }
     }
