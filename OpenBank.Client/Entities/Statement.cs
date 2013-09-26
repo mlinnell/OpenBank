b04c2b29df1ef630ad5ddc483a28203c6c8be602
Matthew Linnell <mlinnell@dwhomes.com>
2013-09-25 21:08:19 -0500

    refactoring

--- OpenBank.Client/Entities/Statement.cs
+++ OpenBank.Client/Entities/Statement.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -11,6 +13,7 @@ namespace OpenBank.Client.Entities
 {
     public class Statement
     {
+        public  const string OutFile = @"C:\temp\credit.out";
         private static TextInfo TextInfo = new CultureInfo("en-US", false).TextInfo;
 
         public Statement()
@@ -22,12 +25,39 @@ namespace OpenBank.Client.Entities
         public Balance AvailableBalance { get; set; }
         public List<Transaction> Transactions { get; set; }
 
+        public static Statement Load(Dictionary<string, string> parameters)
+        {
+            var postData = new StringBuilder();
+            foreach (var kvp in parameters)
+                if (String.IsNullOrEmpty(parameters[kvp.Key]) == false)
+                    postData.AppendFormat("{0}={1}&", kvp.Key, kvp.Value);
+
+            byte[] data = Encoding.ASCII.GetBytes(postData.ToString().TrimEnd('&'));
+
+            var httpReq = (HttpWebRequest)WebRequest.Create("http://localhost:1234/statement");
+            httpReq.Method = "POST";
+            httpReq.ContentType = "application/x-www-form-urlencoded";
+            httpReq.ContentLength = data.Length;
+            httpReq.Accept = "application/json";
+
+            using (var stream = httpReq.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)httpReq.GetResponse();
+            string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            File.WriteAllText(Statement.OutFile, responseString);
+
+            return Statement.Parse(responseString);
+        }
+
         public static Statement Parse(string json)
         {
             if (String.IsNullOrEmpty(json))
                 return new Statement();
 
-            JObject result = JsonConvert.DeserializeObject(json) as JObject;
+            var result = JsonConvert.DeserializeObject(json) as JObject;
 
             var jStatement = result.First.First;
             var statement = new Statement();
--- OpenBank.Client/Program.cs
+++ OpenBank.Client/Program.cs
@@ -15,47 +15,24 @@ namespace OpenBank.Client
 {
     public class Program
     {
-        private const string OutFile = @"C:\temp\credit.out";
-
         public static void Main(string[] args)
         {
             while (true)
             {
                 // Use accounts.sensitive.template as an example
                 // In this git repo, any file ending in ".sensitive" is ignored.
-                JObject hash = JsonConvert.DeserializeObject(File.ReadAllText(@"C:\temp\accounts.sensitive")) as JObject;
+                var jObj = JsonConvert.DeserializeObject(File.ReadAllText(@"C:\temp\accounts.sensitive")) as JObject;
+                var hash = jObj.ToObject<Dictionary<string, string>>();
                 hash["date_end"] = DateTime.Now.ToString("yyyyMMdd");
-                var postParameters = hash.First.First.ToObject<Dictionary<string, string>>();
 
-                StringBuilder postData = new StringBuilder();
-                foreach (var kvp in postParameters)
-                    if (String.IsNullOrEmpty(postParameters[kvp.Key]) == false)
-                        postData.AppendFormat("{0}={1}&", kvp.Key, kvp.Value);
+                string previousJson = File.ReadAllText(Statement.OutFile);
+                var previous = Statement.Parse(previousJson);
+                var current = Statement.Load(hash);
 
-                byte[] data = Encoding.ASCII.GetBytes(postData.ToString().TrimEnd('&'));
-
-                HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create("http://localhost:1234/statement");
-                httpReq.Method = "POST";
-                httpReq.ContentType = "application/x-www-form-urlencoded";
-                httpReq.ContentLength = data.Length;
-                httpReq.Accept = "application/json";
-
-                using (var stream = httpReq.GetRequestStream())
+                if (current.Transactions.Count > previous.Transactions.Count)
                 {
-                    stream.Write(data, 0, data.Length);
-                }
-
-                HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse();
-                string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-
-                string existingJson = File.ReadAllText(OutFile);
-                Statement existing = Statement.Parse(existingJson);
-                Statement updated = Statement.Parse(responseString);
-
-                if (updated.Transactions.Count > existing.Transactions.Count)
-                {
-                    var updatedHash = updated.Transactions.ToDictionary(item => item.ID, item => item);
-                    var existingHash = existing.Transactions.ToDictionary(item => item.ID, item => item);
+                    var updatedHash = current.Transactions.ToDictionary(item => item.ID, item => item);
+                    var existingHash = previous.Transactions.ToDictionary(item => item.ID, item => item);
 
                     var newKeys = updatedHash.Keys.Where(key => existingHash.ContainsKey(key) == false);
                     foreach (var key in newKeys)
@@ -64,8 +41,6 @@ namespace OpenBank.Client
                         File.WriteAllText(@"C:\temp\" + transaction.ID, transaction.ToString());
                     }
                 }
-
-                File.WriteAllText(OutFile, responseString);
 
                 Thread.Sleep(TimeSpan.FromHours(1));
             }
